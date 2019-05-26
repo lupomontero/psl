ded30f0a41d98afbe59993ec8afa929e7021df38
Lupo Montero <lupomontero@gmail.com>
2019-05-26 02:03:07 -0500

    chore(build): Removes event-stream as build dependency

--- data/build.js
+++ data/build.js
@@ -5,8 +5,8 @@
 //
 const Fs = require('fs');
 const Path = require('path');
+const { Transform } = require('stream');
 const Request = require('request');
-const EventStream = require('event-stream');
 const JSONStream = require('JSONStream');
 
 
@@ -23,51 +23,72 @@ internals.dest = Path.join(__dirname, 'rules.json');
 //
 // Parse line (trim and ignore empty lines and comments).
 //
-internals.parseLine = function (line, cb) {
+internals.parseLine = function (line) {
 
   const trimmed = line.trim();
 
   // Ignore empty lines and comments.
   if (!trimmed || (trimmed.charAt(0) === '/' && trimmed.charAt(1) === '/')) {
-    return cb();
+    return;
   }
 
   // Only read up to first whitespace char.
   const rule = trimmed.split(' ')[0];
-  return cb(null, rule);
+  return rule;
 
-  const item = [rule];
-
-  const suffix = rule.replace(/^(\*\.|\!)/, '');
-  const wildcard = rule.charAt(0) === '*';
-  const exception = rule.charAt(0) === '!';
-
-  // If rule has no wildcard or exception we can get away with only one
-  // element in the `item` array.
-  if (suffix === rule && !wildcard && !exception) {
-    return cb(null, item);
-  }
-
-  item.push(suffix);
-
-  if (wildcard) {
-    item.push(true);
-  }
-
-  if (exception) {
-    item.push(true);
-  }
-
-  cb(null, item);
+  // const item = [rule];
+  //
+  // const suffix = rule.replace(/^(\*\.|\!)/, '');
+  // const wildcard = rule.charAt(0) === '*';
+  // const exception = rule.charAt(0) === '!';
+  //
+  // // If rule has no wildcard or exception we can get away with only one
+  // // element in the `item` array.
+  // if (suffix === rule && !wildcard && !exception) {
+  //   return cb(null, item);
+  // }
+  //
+  // item.push(suffix);
+  //
+  // if (wildcard) {
+  //   item.push(true);
+  // }
+  //
+  // if (exception) {
+  //   item.push(true);
+  // }
+  //
+  // cb(null, item);
 };
+
+
+internals.parse = new Transform({
+  objectMode: true,
+  transform(chunk, encoding, cb) {
+    if (this._last === undefined) {
+      this._last = '';
+    }
+
+    this._last += `${chunk}`;
+    const list = this._last.split(/\n/);
+    this._last = list.pop();
+
+    for (let i = 0; i < list.length; i++) {
+      const parsed = internals.parseLine(list[i]);
+      if (parsed) {
+        this.push(parsed);
+      }
+    }
+
+    cb();
+  }
+});
 
 
 //
 // Download rules and create rules.json file.
 //
 Request(internals.src)
-  .pipe(EventStream.split())
-  .pipe(EventStream.map(internals.parseLine))
+  .pipe(internals.parse)
   .pipe(JSONStream.stringify('[', ',', ']'))
   .pipe(Fs.createWriteStream(internals.dest));
-
