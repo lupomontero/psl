36786af35fb922ba707781d4e2daae05480ef656
Travis CI User <travis@example.org>
2019-09-19 16:34:55 +0000

    chore(rules): Updates rules and dist files

--- dist/psl.js
+++ dist/psl.js
@@ -6789,7 +6789,6 @@ module.exports=[
 "mls",
 "mma",
 "mobile",
-"mobily",
 "moda",
 "moe",
 "moi",
@@ -7287,7 +7286,6 @@ module.exports=[
 "العليان",
 "اتصالات",
 "بازار",
-"موبايلي",
 "ابوظبي",
 "كاثوليك",
 "همراه",
@@ -7344,6 +7342,7 @@ module.exports=[
 "barsy.ca",
 "*.compute.estate",
 "*.alces.network",
+"altervista.org",
 "alwaysdata.net",
 "cloudfront.net",
 "*.compute.amazonaws.com",
@@ -8099,6 +8098,7 @@ module.exports=[
 "githubusercontent.com",
 "gitlab.io",
 "glitch.me",
+"lolipop.io",
 "cloudapps.digital",
 "london.cloudapps.digital",
 "homeoffice.gov.uk",
@@ -8786,8 +8786,7 @@ module.exports=[
 "basicserver.io",
 "virtualserver.io",
 "site.builder.nu",
-"enterprisecloud.nu",
-"zone.id"
+"enterprisecloud.nu"
 ]
 },{}],2:[function(require,module,exports){
 /*eslint no-var:0, prefer-arrow-callback: 0, object-shorthand: 0 */
