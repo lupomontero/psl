59c40493eb9e50978502074fe576d8d8924ee1e8
Lupo Montero <lupomontero@gmail.com>
2024-11-08 00:10:25 +0100

    chore(pkg): Replaces dtslint with typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,25 @@
-module.exports = {
-  languageOptions: {
-    ecmaVersion: 2020,
+// @ts-check
+
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  {
+    ...eslint.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+    rules: {
+      indent: [2, 2],
+    },
   },
-  rules: {
-    indent: [2, 2]
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist/*'],
   },
-  ignores: ['dist/*'],
-};
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 0,
+    },
+  }
+);
